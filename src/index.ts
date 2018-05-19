9920f1765181d18a2822d472ec5ae7761470ad70
Kenta Kase <kesin1202000@gmail.com>
2018-05-19 14:37:11 +0900

    Convert to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
+import { firestore } from 'firebase-admin'
+import { CollectionReference, Firestore, QuerySnapshot, Query, DocumentData, DocumentSnapshot } from '@google-cloud/firestore';
+
+declare type Mapping = { [key: string]: string }
+interface DocData {
+  [extra: string]: any
+}
+interface DocObject {
+  id: string,
+  [extra: string]: any
+}
+// declare type DocObject = { id: string, [extra: string]: any}
+
 class FirestoreSimple {
-  constructor (db, collectionPath, mapping) {
+  db: Firestore
+  collectionRef: CollectionReference
+  toDocMapping: Mapping
+  toObjectMapping: Mapping
+
+  constructor (db: Firestore, collectionPath: string, mapping: Mapping) {
     this.db = db
     this.collectionRef = this.db.collection(collectionPath)
     this.toDocMapping = mapping || {}
     this.toObjectMapping = FirestoreSimple._createSwapMapping(this.toDocMapping)
   }
 
-  static _createSwapMapping (mapping) {
-    let swapMap = {}
+  static _createSwapMapping (mapping: Mapping) {
+    let swapMap: Mapping = {}
     Object.keys(mapping).forEach((key) => {
       swapMap[mapping[key]] = key
     })
     return swapMap
   }
 
-  _toDoc (object) {
-    let doc = {}
+  _toDoc (object: DocObject | DocData ) {
+    let doc: DocData = {}
     Object.keys(object).forEach((key) => {
       const toDocKey = this.toDocMapping[key] || key
       doc[toDocKey] = object[key]
@@ -24,9 +42,8 @@ class FirestoreSimple {
     return doc
   }
 
-  _toObject (docId, docData) {
-    let object = {}
-    object['id'] = docId
+  _toObject (docId: string, docData: DocData) {
+    let object: DocObject = { id: docId }
     Object.keys(docData).forEach((key) => {
       const toObjectKey = this.toObjectMapping[key] || key
       object[toObjectKey] = docData[key]
@@ -36,32 +53,32 @@ class FirestoreSimple {
 
   async fetchCollection () {
     const snapshot = await this.collectionRef.get()
-    let arr = []
+    let arr: DocObject[] = []
 
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc: DocumentSnapshot) => {
       arr.push(this._toObject(doc.id, doc.data()))
     })
     return arr
   }
 
-  async fetchByQuery (query) {
+  async fetchByQuery (query: Query) {
     const snapshot = await query.get()
-    let arr = []
+    let arr: DocObject[] = []
 
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc: DocumentSnapshot) => {
       arr.push(this._toObject(doc.id, doc.data()))
     })
     return arr
   }
 
-  async fetchDocument (id) {
+  async fetchDocument (id: string) {
     const snapshot = await this.collectionRef.doc(id).get()
     if (!snapshot.exists) throw new Error(`No document id: ${id}`)
 
     return this._toObject(snapshot.id, snapshot.data())
   }
 
-  async add (object) {
+  async add (object: DocObject | DocData): Promise<DocObject> {
     const doc = this._toDoc(object)
     const docRef = await this.collectionRef.add(doc)
     return {
@@ -70,7 +87,7 @@ class FirestoreSimple {
     }
   }
 
-  async set (object) {
+  async set (object: DocObject) {
     if (!object.id) throw new Error('Argument object must have "id" property')
 
     const docId = object.id
@@ -80,16 +97,16 @@ class FirestoreSimple {
     return object
   }
 
-  async addOrSet (object) {
-    return (!object.id) ? this.add(object) : this.set(object)
+  async addOrSet (object: DocObject | DocData) {
+    return (!object.id) ? this.add(object) : this.set(object as DocObject)
   }
 
-  async delete (docId) {
+  async delete (docId: string) {
     await this.collectionRef.doc(docId).delete()
     return docId
   }
 
-  async bulkSet (objects) {
+  async bulkSet (objects: DocObject[]) {
     const batch = this.db.batch()
 
     objects.forEach((object) => {
@@ -100,10 +117,10 @@ class FirestoreSimple {
     return batch.commit()
   }
 
-  async bulkDelete (docIds) {
+  async bulkDelete (docIds: string[]) {
     const batch = this.db.batch()
 
-    docIds.forEach((docId) => {
+    docIds.forEach((docId: string) => {
       batch.delete(this.collectionRef.doc(docId))
     })
     return batch.commit()
