f3c875f8d9260dc4083bffb70c6ffbfa7193e96a
Kenta Kase <kesin1202000@gmail.com>
2020-03-06 00:27:45 +0900

    chore(test): Skip flaky tests (#122)
    * chore(test): Skip flaky tests
    
    * chore: Disable codecov status

--- __tests__/admin/query_on_snapshot.test.ts
+++ __tests__/admin/query_on_snapshot.test.ts
@@ -21,7 +21,8 @@ const firestore = initFirestore()
 const collectionPath = createRandomCollectionName()
 const firestoreSimple = new FirestoreSimpleAdmin(firestore)
 
-describe('query on_snapshot test', () => {
+// Skip reason: Sometimes real Firestore is unstable so it will be replaced emulator test.
+describe.skip('query on_snapshot test', () => {
   const dao = firestoreSimple.collection<Book, BookDoc>({
     path: collectionPath,
     encode: (book) => {
