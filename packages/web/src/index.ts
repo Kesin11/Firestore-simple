f2d9b26683d6398ca2f99df6579f499a843680e0
Kenta Kase <kesin1202000@gmail.com>
2020-03-28 17:17:46 +0900

    fix: Export 'Query' and revert type only export

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,4 +1,5 @@
 // Web SDK
 export { FirestoreSimple } from './firestore_simple'
-export type { Collection } from './collection'
-export type { Encodable, Decodable } from './types'
+export { Collection } from './collection'
+export { Query } from './query'
+export { Encodable, Decodable } from './types'
